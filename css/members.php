d867c3f4f0cdf42c28d08faf9282db159f0a80c1
sathishkumar <sathish4fri@gmail.com>
2014-12-15 11:38:04 +0530

    changes  in  design
    design not properly aligned

--- css/members.php
+++ css/members.php
@@ -11,24 +11,29 @@ box-shadow: 2px 2px 2px #eee, 0 0 10px #eee;
     font: 18px QuicksandBold,Arial,sans-serif;
     margin-bottom: 15px;
     padding: 12px 7px;
+    
 }
 
 
 .frontpage .front-box-child {
     padding: 0 7px;
-    width: 33%;
+    width: 226px;
 }
 a.front-member-item img.avatar {
-    height: 60px;
-    width: 60px;
+    height: 50px;
+    width: 50px;
 }
 .frontpage .front-member-item img {
     border: 1px solid #e0e0e0;
-    border-radius: 4px;
+    border-radius: 18px;
     box-shadow: 2px 2px 0 0 #e0e0e0;
     margin: 0 12px 12px 0;
     padding: 3px;
 }
 img.avatar {
     float: left;
+}
+.frontpage{
+border-top:1px solid #eee;
+padding-top:10px;
 }
