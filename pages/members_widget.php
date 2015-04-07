6824fc3b3d9bfc38ad8c5135a601a5de16299a12
sathishkumar <sathish4fri@gmail.com>
2015-04-07 18:41:00 +0530

    changing to 10 members

--- pages/members_widget.php
+++ pages/members_widget.php
@@ -15,26 +15,27 @@ $users = $users->getSiteUsers();
 
 //var_dump($users);
 // 
-$i = 20;
+$i = 0;
 ?>
 <div class="frontpage">
 <div class="front-box widget">
 <div class="front-box-title">All Members</div>
 
 <div class="front-box-child">
-<?php foreach($users as $user) {
-      if($i > 20){
-      	continue;
-      }
+<?php foreach($users as $user) {     	
       $user = ossn_user_by_guid($user->guid);
+	
 ?>
 <a title="<?php echo $user->username;?>" class="front-member-item" href="<?php echo ossn_site_url(); ?>u/<?php echo $user->username;?>?ref=ossntb"><img width="60" height="60" alt="Profile picture of Michael" class="avatar user-1-avatar avatar-60 photo" src="<?php echo $user->iconURL()->large;?>"></a>
 				
 <?php 
+$i++;
+ if($i > 11){
+   break;
+  }
 }
 ?>	
 </div>		
 </div><!--front-box-child ends-->
 </div>
-
 
